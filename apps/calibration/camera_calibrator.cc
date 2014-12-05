71445034d2616d0a734a892d2242fffc47154ebf
William McCohen <william.mccohen@gmail.com>
2014-12-05 09:03:29 +0100

    Update camera_calibrator.cc

--- apps/calibration/camera_calibrator.cc
+++ apps/calibration/camera_calibrator.cc
@@ -37,7 +37,7 @@ CameraCalibrator::CameraCalibrator()
 
   if (!capture.open(0)) {
     printf("ERROR: Cannot open cv::VideoCapture\n");
-    return exit(1);
+    exit(1);
   }
   capture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
   capture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
